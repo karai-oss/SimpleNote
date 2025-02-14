87855be2123690a67f3e3a3fc3db4debbfc3fa8b
谢祎 <2518387122@qq.com>
2025-02-14 18:47:29 +0800

    新增工具类

--- app/src/main/java/com/github/simplenotice/utils/EmptyUtils.java
+++ app/src/main/java/com/github/simplenotice/utils/EmptyUtils.java
@@ -1,6 +1,8 @@
 package com.github.simplenotice.utils;
 
 
+import java.util.Collection;
+
 /**
  * 判空工具类
  *
@@ -9,7 +11,31 @@ package com.github.simplenotice.utils;
  */
 public class EmptyUtils {
 
-    public static boolean object(Object o){
+    /**
+     * 判断一个对象是否为空
+     * @param o 对象
+     * @return true 为空  false 不为空
+     */
+    public static boolean object(Object o) {
         return o == null || "".equals(o);
+    }
+
+    /**
+     * 判断一个字符串是否为空
+     * @param str 字符串
+     * @return true 为空  false 不为空
+     */
+    public static boolean string(String str) {
+        return str == null || "".equals(str) || str.length() == 0;
+    }
+
+
+    /**
+     * 判断一个集合是否为空
+     * @param collection 集合
+     * @return true 为空  false 不为空
+     */
+    public static boolean collection(Collection collection) {
+        return collection == null || collection.size() == 0;
     }
 }
